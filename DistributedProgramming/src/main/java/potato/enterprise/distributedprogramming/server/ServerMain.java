21ec20554343f3ce415335cb035b5a87fb982047
mcarr <mcarr@LAPTOP-L8HM1LUB>
2021-03-06 19:09:03 +0100

    Implementación del método main del servidor.

--- DistributedProgramming/src/main/java/potato/enterprise/distributedprogramming/server/ServerMain.java
+++ DistributedProgramming/src/main/java/potato/enterprise/distributedprogramming/server/ServerMain.java
@@ -1,5 +1,12 @@
 package potato.enterprise.distributedprogramming.server;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author mcarr
@@ -10,7 +17,21 @@ public class ServerMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Socket socket = null;
+        
+        try(ServerSocket server = new ServerSocket()) {
+            InetSocketAddress addr = new InetSocketAddress("localhost", 5555);
+            server.bind(addr);
+            
+            while(true) {
+                socket = server.accept();
+                System.out.println("Jugador conectado al servidor");
+                Petition petition = new Petition(socket);
+                petition.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
 }
