15f4e50c57e9abd7cbc982253ff5906dfb884edc
mcarr <mcarr@LAPTOP-L8HM1LUB>
2021-03-07 13:28:27 +0100

    Modificación del getter getGameHost.

--- DistributedProgramming/src/main/java/potato/enterprise/distributedprogramming/server/Game.java
+++ DistributedProgramming/src/main/java/potato/enterprise/distributedprogramming/server/Game.java
@@ -51,7 +51,7 @@ public class Game {
     }
 
     public Player getGameHost() {
-        return gameHost;
+        return playerList.get(0);
     }
 
     public void setGameHost(Player gameHost) {
