c87b2c6193928ea76932cc77940cfe52025a7142
mcarr <mcarr@LAPTOP-L8HM1LUB>
2021-03-07 13:29:17 +0100

    Inclusión de javadoc para la clase PlayerLauncher.

--- DistributedProgramming/src/main/java/potato/enterprise/distributedprogramming/clientsimulator/PlayerLauncher.java
+++ DistributedProgramming/src/main/java/potato/enterprise/distributedprogramming/clientsimulator/PlayerLauncher.java
@@ -7,13 +7,15 @@ package potato.enterprise.distributedprogramming.clientsimulator;
 public class PlayerLauncher {
 
     /**
+     * Generador de 10 jugadores con sus identificadores.
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             Player player = new Player(i);
             player.start();
         }
     }
-    
+
 }
